3f2993abd3d02a16fe7173d99df30ba4bf2b11b3
Roma Nesterenko <rom.puh@gmail.com>
2024-06-03 17:56:35 +0300

    make models Topic, Newspaper and Redactor. Also applied 'black' to agency, newspaper_agency and manage.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from newspaper_agency.settings import AUTH_USER_MODEL
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, related_name="topic", on_delete=models.CASCADE)
+    publisher = models.ManyToManyField(AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.title
