66c8a22a1d132d5c82c4d406e21eba8e6b6fffa8
Roma Nesterenko <rom.puh@gmail.com>
2024-06-04 11:45:46 +0300

    made index in the views and created empty index.html

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Newspaper, Redactor, Topic
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    num_newspapers = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+    num_visits = request.session.get("nim_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+    contex = {
+        "num_newspapers": num_newspapers,
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_visits": num_visits,
+    }
+    return render(request, "agency/index.html", context=contex )
