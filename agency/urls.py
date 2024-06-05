9c08f135a1927f02a34cf2e8792d6a8c78e9164a
Roma Nesterenko <rom.puh@gmail.com>
2024-06-05 09:54:32 +0300

    made newspaper_list.html, added path to it in agency/urls.py and added reference to it in sidebar

--- agency/urls.py
+++ agency/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import (index)
+from .views import (
+    index,
+    NewspaperListView,
+)
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("newspapers/", NewspaperListView.as_view(), name="newspaper_list")
 ]
 
 
