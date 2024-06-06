2144da14edfde6e3ba263684cda1e74ab2dc6ba5
Roma Nesterenko <rom.puh@gmail.com>
2024-06-06 22:18:20 +0300

    added paths to TopicVievs and fixed path to RedactorVievs in urls.py

--- agency/urls.py
+++ agency/urls.py
@@ -12,6 +12,10 @@ from .views import (
     RedactorUpdateView,
     RedactorDetailView,
     RedactorDeleteView,
+    TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
 )
 
 
@@ -22,12 +26,15 @@ urlpatterns = [
     path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper_create"),
     path("newspapers/<int:pk>/update/", NewspaperUpdateView.as_view(), name="newspaper_update"),
     path("newspapers/<int:pk>/delete/", NewspaperDeleteView.as_view(), name="newspaper_delete"),
-    path("redactor/", RedactorListView.as_view(), name="redactor_list"),
-    path("redactor/create/", RedactorCreateView.as_view(), name="redactor_create"),
-    path("redactor/<int:pk>/update/", RedactorUpdateView.as_view(), name="redactor_update"),
-    path("redactor/<int:pk>/", RedactorDetailView.as_view(), name="redactor_detail"),
-    path("redactor/<int:pk>/delete/", RedactorDeleteView.as_view(), name="redactor_delete"),
-
+    path("redactors/", RedactorListView.as_view(), name="redactor_list"),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor_create"),
+    path("redactors/<int:pk>/update/", RedactorUpdateView.as_view(), name="redactor_update"),
+    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor_detail"),
+    path("redactors/<int:pk>/delete/", RedactorDeleteView.as_view(), name="redactor_delete"),
+    path("topics/", TopicListView.as_view(), name="topic_list"),
+    path("topics/create/", TopicCreateView.as_view(), name="topics_create"),
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topics_update"),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topics_delete"),
 ]
 
 
