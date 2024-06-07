82d2827f432830c3716a07352a04bc36aca446c3
Roma Nesterenko <rom.puh@gmail.com>
2024-06-07 11:07:25 +0300

    made SearchForms for each lists in forms.py

--- agency/forms.py
+++ agency/forms.py
@@ -35,3 +35,42 @@ class RedactorUpdateForm(forms.ModelForm):
             "last_name",
             "years_of_experience",
         )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by title"
+            }
+        )
+    )
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by username"
+            }
+        )
+    )
